a982d28ae59a7dbbbedccf038b24a355624cdfc3
pinky rikky <georgechizzy29@gmail.com>
2023-09-30 16:01:54 +0100

    added animation to about page

--- src/pages/about/AboutUs.jsx
+++ src/pages/about/AboutUs.jsx
@@ -1,30 +1,110 @@
 import React from 'react'
 import '../../assets/css/about/About.css'
+import CountUp from 'react-countup';
+import 'animate.css';
+
+
 
 export default function AboutUs() {
   return (
     <>
         <h2 className='About'>About Us</h2>
         <div className='AboutUs'>
-            <div className='happyClient'>
+            <div className='happyClient aboutMe animate__animated animate__fadeInUp animate__faster'>
+
+            
                 <div>
-                    <h1>1,000</h1>
+                    <h1><CountUp
+  start={0}
+  end={1}
+  duration={2.75}
+  separator=" "
+  decimals={3}
+  decimal=","
+ 
+  onEnd={() => console.log('Ended! 👏')}
+  onStart={() => console.log('Started! 💨')}
+>
+  {({ countUpRef, start }) => (
+    <div>
+      <span ref={countUpRef} />
+     
+    </div>
+  )}
+</CountUp></h1>
                     <p>Happy Clients</p>
                 </div>
                 <div>
-                    <h1>200</h1>
+                    <h1>
+                    <CountUp
+  start={0}
+  end={2}
+  duration={2.75}
+  separator=" "
+  decimals={2}
+  decimal=""
+ 
+  onEnd={() => console.log('Ended! 👏')}
+  onStart={() => console.log('Started! 💨')}
+>
+  {({ countUpRef, start }) => (
+    <div>
+      <span ref={countUpRef} />
+     
+    </div>
+  )}
+</CountUp>
+                    </h1>
                     <p>Projects Done</p>
                 </div>
                 <div>
-                    <h1>500</h1>
+                    <h1>
+                    <CountUp
+  start={0}
+  end={5}
+  duration={2.75}
+  separator=" "
+  decimals={2}
+  decimal=""
+ 
+  onEnd={() => console.log('Ended! 👏')}
+  onStart={() => console.log('Started! 💨')}
+>
+  {({ countUpRef, start }) => (
+    <div>
+      <span ref={countUpRef} />
+     
+    </div>
+  )}
+</CountUp>
+                    </h1>
                     <p>Cups Of Coffee </p>
                 </div>
                 <div>
-                    <h1>587</h1>
+                    <h1>
+                    <CountUp
+  start={0}
+  end={5.87}
+  duration={2.75}
+  separator=" "
+  decimals={2}
+  decimal=""
+ 
+  onEnd={() => console.log('Ended! 👏')}
+  onStart={() => console.log('Started! 💨')}
+>
+  {({ countUpRef, start }) => (
+    <div>
+      <span ref={countUpRef} />
+     
+    </div>
+  )}
+</CountUp>
+                    </h1>
                     <p>Working Hours</p>
                 </div>
             </div>
-            <div className='aboutMe'>
+            <div className='aboutMe animate__animated animate__fadeInUp animate__delay-0.5s'>
                 <h4>ABOUT ME</h4>
                 <h1>A Web  Designer & Developer Based In Nigeria</h1>
                 <p>I am a Web Designer.  I specialize in Javascript, typescript, react and Nextjs. </p>
